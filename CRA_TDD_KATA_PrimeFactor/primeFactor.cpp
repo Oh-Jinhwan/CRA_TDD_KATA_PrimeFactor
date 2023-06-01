5a81d94d472f2ef3ed2662a4ad0c923c17725761
jinhwan.oh <ognan98@gmail.com>
2023-06-01 14:59:47 +0900

    [feature] page33

--- CRA_TDD_KATA_PrimeFactor/primeFactor.cpp
+++ CRA_TDD_KATA_PrimeFactor/primeFactor.cpp
@@ -1,4 +1,12 @@
+#include <vector>
+using namespace std;
+
 class PrimeFactor
 {
-	
+public:
+	vector<int> of(int value)
+	{
+		vector<int> result;
+		return result;
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
 #include "../CRA_TDD_KATA_PrimeFactor/primeFactor.cpp"
+#include <vector>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(TestCaseName, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
